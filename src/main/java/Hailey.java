5b9eb5a1f362215e061cd5bb596e72db469d0f57
kylie luk <e0969639@u.nus.edu>
2025-01-23 11:28:06 +0800

    Add support for echo

--- src/main/java/Hailey.java
+++ src/main/java/Hailey.java
@@ -1,8 +1,26 @@
+import java.util.Scanner;
+
 public class Hailey {
+    static String line = "____________________________________________________________\n";
     public static void main(String[] args) {
-        String line = "____________________________________________________________\n";
         System.out.println(line + "Hello! I'm Hailey :)\n"
             + "What can I do for you?\n" + line);
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println(line);
+                exit();
+                break;
+            } else {
+                System.out.println(input +"\n" + line);
+            }
+        }
+        scanner.close();
+    }
+
+    static void exit() {
         System.out.println("Bye. Hope to see you again soon!\n" + line);
     }
 }
